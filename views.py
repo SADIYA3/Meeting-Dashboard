d417a169a08aed0eb33aeac6a00efc5bb4a7d52a
SADIYA3 <146457920+SADIYA3@users.noreply.github.com>
2025-06-02 20:00:47 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from .models import Meeting
+from .serializers import MeetingSerializer
+
+class MeetingListCreate(generics.ListCreateAPIView):
+    queryset = Meeting.objects.all()
+    serializer_class = MeetingSerializer
